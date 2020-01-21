4bf3928377809ec4ecd4591118fd8a4ffb9494b3
Cyprian Bowen <branco1lelgo@gmail.com>
2020-01-21 16:50:07 +0300

    add user login details

--- app/src/main/java/com/moringaschool/homeautomation/UserInterface/LoginActivity.java
+++ app/src/main/java/com/moringaschool/homeautomation/UserInterface/LoginActivity.java
@@ -2,15 +2,32 @@ package com.moringaschool.homeautomation.UserInterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.moringaschool.homeautomation.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        if(view == mRegisterTextView){
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
