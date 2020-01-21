5fb4b163ce2354284924f95f8fc271a07516bfef
Cyprian Bowen <branco1lelgo@gmail.com>
2020-01-21 16:49:34 +0300

    update user details for creating an account

--- app/src/main/java/com/moringaschool/homeautomation/UserInterface/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/homeautomation/UserInterface/CreateAccountActivity.java
@@ -1,16 +1,74 @@
 package com.moringaschool.homeautomation.UserInterface;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.homeautomation.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String TAG = CreateAccountActivity.class.getSimpleName();
+    @BindView(R.id.createUserButton) Button mCreateUserButton;
+    @BindView(R.id.nameEditText) EditText mNameEditText;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @BindView(R.id.loginTextView) TextView mLoginTextView;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+        mCreateUserButton.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public  void onClick(View view){
+        if(view == mLoginTextView){
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        if (view == mCreateUserButton){
+            createNewUser();
+        }
+    }
+    private void createNewUser(){
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "Authentication successful");
+                        }else{
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
