7855829347b0298cd5343372543b729b31c92796
Cyprian Bowen <branco1lelgo@gmail.com>
2020-01-21 15:45:10 +0300

    initial commit

---
+++ app/src/main/java/com/moringaschool/homeautomation/UserInterface/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.homeautomation.UserInterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.homeautomation.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
---
+++ app/src/main/java/com/moringaschool/homeautomation/UserInterface/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.homeautomation.UserInterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.homeautomation.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
