adb7607503ef9d3df95779c046637e81014da1c9
Cyprian Bowen <branco1lelgo@gmail.com>
2020-01-23 23:44:52 +0300

    create logic for bluetooth communication

---
+++ app/src/main/java/com/moringaschool/homeautomation/UserInterface/ControlRoomActivity.java
@@ -0,0 +1,152 @@
+package com.moringaschool.homeautomation.UserInterface;
+
+
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.moringaschool.homeautomation.R;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ControlRoomActivity extends Fragment {
+    @BindView(R.id.room1Button) Button button1On;
+    @BindView(R.id.room1OffButton) Button button1Off;
+    @BindView(R.id.room2Button) Button button2On;
+    @BindView(R.id.room2OffButton) Button button2Off;
+    @BindView(R.id.unPairButton) Button unPairButton;
+
+    String address = null;
+    private ProgressDialog progressDialog;
+    BluetoothAdapter mBluetooth = null;
+    BluetoothSocket bluetoothSocket = null;
+    private boolean isConnected = false;
+    static final UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+
+    public ControlRoomActivity() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_control_room, container, false);
+        ButterKnife.bind(this, view);
+        Intent newIntent = getActivity().getIntent();
+        address = newIntent.getStringExtra(DevicesActivity.EXTRA_ADDRESS);
+
+        new ConnectBlueTooth().execute();
+
+        button1On.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSignal("1");
+            }
+        });
+        button1Off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSignal("2");
+            }
+        });
+        button2On.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSignal("3");
+            }
+        });
+        button2Off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSignal("4");
+            }
+        });
+        unPairButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Disconnect();
+            }
+        });
+
+    return view;
+    }
+    private void sendSignal (String number){
+        if(bluetoothSocket != null){
+            try{
+                bluetoothSocket.getOutputStream().write(number.toString().getBytes());
+            } catch (IOException e){
+                msg("Error");
+            }
+        }
+    }
+    private void Disconnect(){
+        if(bluetoothSocket !=null){
+            try {
+                bluetoothSocket.close();
+            }catch (IOException e){
+                msg("Error");
+            }
+        }
+    }
+    private void msg (String s){
+        Toast.makeText(getContext().getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+    private class ConnectBlueTooth extends AsyncTask<Void, Void, Void >{
+        private boolean ConnectSuccess = true;
+
+        /*@Override
+        protected void onPreExecute(){
+            progressDialog = ProgressDialog.show(getActivity(), "Connecting...", "Please wait!!");
+        }*/
+        @Override
+        protected Void doInBackground (Void... devices){
+            try{
+                if(bluetoothSocket==null || !isConnected){
+                    mBluetooth = BluetoothAdapter.getDefaultAdapter();
+                    BluetoothDevice bluetoothDevice = mBluetooth.getRemoteDevice(address);
+                    bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(mUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    bluetoothSocket.connect();
+                }
+            }catch (IOException e){
+                ConnectSuccess = false;
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void results){
+            super.onPostExecute(results);
+            if(!ConnectSuccess){
+                msg("Connection failed. Try again.");
+            }else{
+                msg("Connected");
+                isConnected = true;
+            }
+            progressDialog.dismiss();
+        }
+    }
+}
---
+++ app/src/main/java/com/moringaschool/homeautomation/UserInterface/DevicesActivity.java
@@ -0,0 +1,95 @@
+package com.moringaschool.homeautomation.UserInterface;
+
+
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.moringaschool.homeautomation.R;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DevicesActivity extends Fragment {
+    @BindView(R.id.pairButton) Button buttonPaired;
+    @BindView(R.id.devicesListView) ListView deviceList;
+
+    private BluetoothAdapter mBluetooth = null;
+    private Set<BluetoothDevice> pairedDevices;
+    public static String EXTRA_ADDRESS = "device_address";
+    /*public DevicesActivity() {
+    }*/
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_devices, container, false);
+        ButterKnife.bind(this, view);
+
+        mBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetooth==null){
+            Toast.makeText(getActivity().getApplicationContext(), "Bluetooth device not available", Toast.LENGTH_LONG).show();
+        } else if (mBluetooth.isEnabled()){
+            Intent turnBluetoothOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnBluetoothOn, 1);
+        }
+        buttonPaired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pairedDevicesList();
+            }
+        });
+        return view;
+    }
+    private void pairedDevicesList(){
+        pairedDevices = mBluetooth.getBondedDevices();
+        ArrayList list = new ArrayList();
+
+        if(pairedDevices.size() > 0){
+            for(BluetoothDevice bt : pairedDevices){
+                list.add(bt.getName().toString() + "\n" + bt.getAddress().toString());
+            }
+        } else {
+            Toast.makeText(getContext().getApplicationContext(), "No paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        }
+        Context context;
+        final ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list);
+        deviceList.setAdapter(adapter);
+        deviceList.setOnItemClickListener(myListClickListener);
+    }
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String info = ((TextView) view).getText().toString();
+            String address = info.substring(info.length()-17);
+
+            Intent intent = new Intent(getActivity(), ControlRoomActivity.class);
+            intent.putExtra(EXTRA_ADDRESS, address);
+            startActivity(intent);
+        }
+    };
+
+}
